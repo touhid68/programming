09c286464e3880e8b346fac8b240b0d6754cb7c1
Md. Mossadek Touhid <58106132+touhid68@users.noreply.github.com>
2023-08-01 11:01:37 +0600

    binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -6,8 +6,8 @@ void binary_search(int a[],int n)
     int item;
     printf("\n(Binary search)The searching item is: ");
     scanf("%d",&item);
-    int low_index=1;
-    int high_index=n;
+    int low_index=1; //if 0 based index, then here 0
+    int high_index=n; // then here (n-1)
     int mid_index;
     while(low_index<=high_index)
     {
@@ -37,12 +37,12 @@ int main()
     int n=10;
     int a[n],i;
     printf("Initial Array:\n");
-    for(i=1; i<=n; i++)
+    for(i=1; i<=n; i++) // i=0; i<n; i++
     {
         cin>>a[i];
         //a[i]=(rand()%5)+1;
     }
-    sort(a+0,a+n+1);
+    sort(a+0,a+n+1); // if 0 based index, sort(a,a+n)
     for(i=0; i<n; i++)
     {
         cout<<a[i]<<" ";
